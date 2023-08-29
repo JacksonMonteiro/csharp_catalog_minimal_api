56fa95d2ddde4125777ce88ba296044221e3766d
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2023-08-29 10:20:10 -0300

    Context class

---
+++ CatalogMinimalAPI/Context/CatalogMinimalAPIContext.cs
@@ -0,0 +1,11 @@
+﻿using CatalogMinimalAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogMinimalAPI.Context {
+    public class CatalogMinimalAPIContext : DbContext {
+        public CatalogMinimalAPIContext(DbContextOptions<CatalogMinimalAPIContext> options) : base(options) { }
+
+        public DbSet<Product>? Produts { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+    }
+}
