ce385c8d48c02b9787c3c2b47c5db7b15b01ec8d
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2023-08-29 11:45:00 -0300

    Registering db context service and connection string

--- CatalogMinimalAPI/Context/CatalogMinimalAPIContext.cs
+++ CatalogMinimalAPI/Context/CatalogMinimalAPIContext.cs
@@ -1,5 +1,5 @@
 ﻿using CatalogMinimalAPI.Models;
-using Microsoft.EntityFrameworkCore;
+ using Microsoft.EntityFrameworkCore;
 
 namespace CatalogMinimalAPI.Context {
     public class CatalogMinimalAPIContext : DbContext {
@@ -7,5 +7,22 @@ namespace CatalogMinimalAPI.Context {
 
         public DbSet<Product>? Produts { get; set; }
         public DbSet<Category>? Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Category>().HasKey(c => c.Id);
+            modelBuilder.Entity<Category>().Property(c => c.Name).HasMaxLength(100).IsRequired();
+            modelBuilder.Entity<Category>().Property(c => c.Description).HasMaxLength(150).IsRequired();
+
+            modelBuilder.Entity<Product>().HasKey(p => p.Id);
+            modelBuilder.Entity<Product>().Property(p => p.Name).HasMaxLength(100).IsRequired();
+            modelBuilder.Entity<Product>().Property(p => p.Description).HasMaxLength(150);
+            modelBuilder.Entity<Product>().Property(p => p.ImagePath).HasMaxLength(100);
+            modelBuilder.Entity<Product>().Property(p => p.Price).HasPrecision(14, 2);
+
+            modelBuilder.Entity<Product>()
+                .HasOne<Category>(c => c.Category)
+                    .WithMany(p => p.Products)
+                        .HasForeignKey(c => c.CategoryId);
+        }
     }
 }
--- CatalogMinimalAPI/Program.cs
+++ CatalogMinimalAPI/Program.cs
@@ -1,8 +1,14 @@
+using CatalogMinimalAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<CatalogMinimalAPIContext>(options => options.UseMySql(connectionString));
 
 var app = builder.Build();
 
