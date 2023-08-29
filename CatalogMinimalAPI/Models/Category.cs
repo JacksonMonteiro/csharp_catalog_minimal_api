8ec160d803a58162793775a9708854a4568cb81e
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2023-08-29 09:51:22 -0300

    Category and Product model classes

---
+++ CatalogMinimalAPI/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace CatalogMinimalAPI.Models {
+    public class Category {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
