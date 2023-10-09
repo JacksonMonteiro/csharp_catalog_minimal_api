8439526f18693df269e462ad1c0562620204ca71
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2023-10-09 15:19:31 -0300

    get all categories and get category by id routes

--- CatalogMinimalAPI/Program.cs
+++ CatalogMinimalAPI/Program.cs
@@ -16,18 +16,26 @@ var app = builder.Build();
 // API Endpoints
 app.MapGet("/", () => "Catalog Minimal API - 2022");
 
+// Categories
 app.MapPost("/categories", async (Category category, CatalogMinimalAPIContext db) => {
     db.Categories.Add(category);
     await db.SaveChangesAsync();
     return Results.Created($"/categories/{category.Id}", category);
 });
 
+app.MapGet("/categories", async (CatalogMinimalAPIContext db) => await db.Categories.ToListAsync());
+
+app.MapGet("/categories/{id:int}", async (int id, CatalogMinimalAPIContext db) => {
+    return await db.Categories.FindAsync(id) is Category category ? Results.Ok(category) : Results.NotFound();
+
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
-} 
+}
 
-app.UseHttpsRedirection(); 
+app.UseHttpsRedirection();
 
 app.Run();
