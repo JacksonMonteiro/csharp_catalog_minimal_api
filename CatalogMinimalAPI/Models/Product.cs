8d03aae7621b0d893ff8fd215c6f83b5422b9010
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2023-10-09 16:04:22 -0300

    Products endpoints

--- CatalogMinimalAPI/Models/Product.cs
+++ CatalogMinimalAPI/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace CatalogMinimalAPI.Models {
+﻿using System.Text.Json.Serialization;
+
+namespace CatalogMinimalAPI.Models {
     public class Product {
         public int Id { get; set; }
         public string? Name { get; set; }
@@ -8,6 +10,7 @@
         public DateTime PurchaseDate { get; set; }
         public int Stock { get; set; }
         public int CategoryId { get; set; }
+        [JsonIgnore]
         public Category? Category { get; set; }
     }
 }
--- CatalogMinimalAPI/Program.cs
+++ CatalogMinimalAPI/Program.cs
@@ -57,6 +57,53 @@ app.MapDelete("/categories/{id:int}", async (int id, CatalogMinimalAPIContext db
     return Results.NoContent();
 });
 
+// Products
+app.MapPost("/products", async (Product product, CatalogMinimalAPIContext db) => {
+    db.Produts.Add(product);
+    await db.SaveChangesAsync();
+    return Results.Created($"/categories/{product.Id}", product);
+});
+
+app.MapGet("/products", async (CatalogMinimalAPIContext db) => await db.Produts.ToListAsync());
+
+app.MapGet("/products/{id:int}", async (int id, CatalogMinimalAPIContext db) => {
+    return await db.Produts.FindAsync(id) is Product product ? Results.Ok(product) : Results.NotFound();
+
+});
+
+app.MapPut("/products/{id:int}", async (int id, Product product, CatalogMinimalAPIContext db) => {
+    if (product.Id != id) {
+        return Results.BadRequest();
+    }
+
+    var productDB = await db.Produts.FindAsync(id);
+    if (productDB is null) return Results.NotFound();
+
+    productDB.Name = product.Name;
+    productDB.Description = product.Description;
+    productDB.Price = product.Price;
+    productDB.ImagePath = product.ImagePath;
+    productDB.PurchaseDate = product.PurchaseDate;
+    productDB.Stock = product.Stock;
+    productDB.CategoryId = product.CategoryId;
+
+
+    await db.SaveChangesAsync();
+    return Results.Ok(productDB);
+});
+
+app.MapDelete("/products/{id:int}", async (int id, CatalogMinimalAPIContext db) => {
+    var product = await db.Produts.FindAsync(id);
+    if (product  is null) {
+        return Results.NotFound();
+    }
+
+    db.Produts.Remove(product);
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
