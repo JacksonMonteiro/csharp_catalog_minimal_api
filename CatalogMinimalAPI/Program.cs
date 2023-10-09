16f46129c32cb0bcf666a44752313f7f298a6f6b
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2023-10-09 15:08:12 -0300

    Initial Migration and First route

--- CatalogMinimalAPI/Program.cs
+++ CatalogMinimalAPI/Program.cs
@@ -8,15 +8,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("Default");
-builder.Services.AddDbContext<CatalogMinimalAPIContext>(options => options.UseMySql(connectionString));
+builder.Services.AddDbContext<CatalogMinimalAPIContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
+
+// API Endpoints
+app.MapGet("/", () => "Catalog Minimal API - 2022");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+} 
 
 app.UseHttpsRedirection();
 
